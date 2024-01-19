0d3c674331846af59006dac8bfaa1dd1c7291774
asif809999 <asif809999@gmail.com>
2024-01-19 19:32:41 +0600

    exit(0) implementation inside ifinite loop and switch case lecture8 home task 1 codehelp

---
+++ exitFunction.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number =1;
+   
+    while(1){
+        switch (number)
+        {
+        case 1 : exit(0);
+            break;
+        
+        default:
+            break;
+        }
+    }
+   
+}
