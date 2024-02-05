014b87832421451e81545df4f4ed8f05f7df3565
asif809999 <asif809999@gmail.com>
2024-02-05 16:13:45 +0600

    odd number of array printed by swaping

--- swapOddNumberOfArray.cpp
+++ swapOddNumberOfArray.cpp
@@ -6,24 +6,27 @@ void swapArrayFunction(int arr[], int size)
 {
     for (int i = 0; i < size; i = i + 2)
     {
-        swap(arr[i], arr[i + 1]);
+        if (i + 1 < size)
+        {
+            swap(arr[i], arr[i + 1]);
+        }
     }
 }
 void printArrayFunction(int arr[], int n)
 {
     for (int j = 0; j < n; j++)
     {
-        cout << arr[j]<<" ";
+        cout << arr[j] << " ";
     }
 }
 
 int main()
 
 {
-    int evenNumberOfArray[8] = {3, 5, 1, 0, 8, 5, 2, 9};
+    int oddNumberOfArray[7] = {3, 5, 1, 0, 8, 5, 2};
 
-    swapArrayFunction(evenNumberOfArray, 8);
-    printArrayFunction(evenNumberOfArray, 8);
+    swapArrayFunction(oddNumberOfArray, 7);
+    printArrayFunction(oddNumberOfArray, 7);
 
     return 0;
 }
