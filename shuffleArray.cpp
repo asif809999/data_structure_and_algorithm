095418175952b5bee2a56c5d5529e7cbae22d2bf
asif809999 <asif809999@gmail.com>
2024-05-22 07:37:08 +0600

    shuffle the array; leetcode 1470

---
+++ shuffleArray.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+#include <bits/stdc++.h>
+
+vector<int> shuffle(vector<int> &nums, int n)
+{
+    vector<int> ans;
+    for (int i = 0; i < n; i++)
+    {
+        ans.push_back(nums[i]);
+        ans.push_back(nums[i + n]);
+    }
+    return ans;
+}
+
+int main()
+{
+}
